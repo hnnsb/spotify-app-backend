9ba4225cc14c546a57db40895512b1ce604b30ea
hnnsb <hannes.b@wtnet.de>
2023-02-09 23:08:12 +0100

    Init project

---
+++ src/test/java/de/hnnsb/spotifyappbackend/SpotifyAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hnnsb.spotifyappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
